880e7fd49df424a0a3243dc785fee924eebe83b1
Lou Pruitt <Lpruitt2004@gmail.com>
2018-07-13 15:16:26 -0700

    Create account mostly done mostly

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,20 @@
 import types from './types';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+
+export function createAccount(userInfo){
+    return async (dispatch) => {
+        const resp = await axios.post(`${BASE_URL}/signup`, userInfo);
+
+        console.log('Sign Up Response:', resp);
+    }
+}
 
 export function signIn(){
-    return {type: types.SIGN_IN};
+    return { type: types.SIGN_IN };
 }
 
 export function signOut(){
-    return {type: types.SIGN_OUT};
+    return { type: types.SIGN_OUT };
 }
