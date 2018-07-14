a8f74a17d53f96f0507ad6d23c5c8754480b3591
Lou Pruitt <Lpruitt2004@gmail.com>
2018-07-13 17:42:18 -0700

    added random movie quotes

--- src/components/movie_quote.js
+++ src/components/movie_quote.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import auth from '../hoc/auth';
+import { connect } from 'react-redux';
+import { getMovieQuote } from '../actions';
 
-const MovieQuote = props => {
-    return (
-        <div>
-            <h1 className="text-center">Movie Quote</h1>
-            <h4>I haven't had a carb since '04</h4>
-        </div>
-    );
+class MovieQuote extends Component {
+        componentDidMount(){
+            this.props.getMovieQuote();
+        }
+
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">Movie Quote</h1>
+                <h4>{this.props.quote}</h4>
+            </div>
+        );
+    }
 }
 
-export default auth(MovieQuote);
+function mapStateToProps(state){
+    return {
+        quote: state.movie.quote
+    }
+}
+
+export default connect(mapStateToProps, { getMovieQuote: getMovieQuote })(auth(MovieQuote));
